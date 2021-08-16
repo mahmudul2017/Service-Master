93d8c00aa92690e6e88ac6ccd4a91f3d58daf7fd
mahmudul2017 <mahmudulhasanshah@gmail.com>
2021-08-16 16:05:48 +0600

    + Time Compare

--- app/src/main/java/com/gorentalbd/service/ServiceActivity.kt
+++ app/src/main/java/com/gorentalbd/service/ServiceActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gorentalbd.service.simpleService.ServiceActivity1
+import com.gorentalbd.service.timeDifference.TimeDifferenceActivity
 import kotlinx.android.synthetic.main.activity_service.*
 
 class ServiceActivity : AppCompatActivity() {
@@ -13,6 +14,10 @@ class ServiceActivity : AppCompatActivity() {
 
         btnSimpleService.setOnClickListener {
             startActivity(Intent(this, ServiceActivity1::class.java))
+        }
+
+        btnTimeDifference.setOnClickListener {
+            startActivity(Intent(this, TimeDifferenceActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/gorentalbd/service/timeDifference/TimeDifferenceActivity.kt
@@ -0,0 +1,51 @@
+package com.gorentalbd.service.timeDifference
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.gorentalbd.service.R
+import kotlinx.android.synthetic.main.activity_timedifference.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class TimeDifferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timedifference)
+
+        /*val date1: Date = Calendar.getInstance().time
+        Log.i("app", "Date1 value is: $date1")
+        val date2: Date = Calendar.getInstance().time
+        Log.i("app", "Date2 value is: $date2")
+
+        if (date1.compareTo(date2) > 0) {
+            Log.i("app", "Date1 is after Date2")
+        } else if (date1.compareTo(date2) < 0) {
+            Log.i("app", "Date1 is before Date2")
+        } else if (date1.compareTo(date2) === 0) {
+            Log.i("app", "Date1 is equal to Date2")
+        }*/
+
+        val c = Calendar.getInstance()
+        val sdf = SimpleDateFormat("HH:mm")
+        val getCurrentTime: String = sdf.format(c.time)
+        val getFajrTime = "16.01"
+        val getDuhurTime = "16.03"
+        val getAzorTime = "16.05"
+
+        if (getCurrentTime.compareTo(getFajrTime) < 0) { // = getCurrentTime < getFajrTime
+            // Do your staff
+            tvAlarm.text = "Fajr Time"
+            Log.d("Return", "Fojor: getTestTime > getCurrentTime Current: $getCurrentTime Test: $getFajrTime")
+        } else if (getCurrentTime.compareTo(getDuhurTime) < 0) {
+            tvAlarm.text = "Duhur Time"
+            Log.d("Return", "Duhur: getTestTime < getCurrentTime Current: $getCurrentTime Test: $getFajrTime")
+        } else if (getCurrentTime.compareTo(getAzorTime) < 0) {
+            tvAlarm.text = "Asor Time"
+            Log.d("Return", "Asor: getTestTime < getCurrentTime Current: $getCurrentTime Test: $getFajrTime")
+        } else {
+            tvAlarm.text = "No Time"
+            Log.d("Return", "No: No Namz Time")
+        }
+    }
+}
